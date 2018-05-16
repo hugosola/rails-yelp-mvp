85dd374bfd982e19a4df66021bb9eb937297df3d
hugo sola <hugo.degeorges@gmail.com>
2018-05-16 21:36:30 +0200

    mynrestaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0102030405',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0102030405',
+    category:     'italian'
+  },
+  {
+    name:         'Mama Roma',
+    address:      '19 rue Oberkampf',
+    phone_number: '0102030405',
+    category:     'italian'
+  },
+  {
+    name:         'La Brigade',
+    address:      '23 rue Saint Maur',
+    phone_number: '0102030405',
+    category:     'french'
+  },
+  {
+    name:         'Pitaya',
+    address:      '16 Villa Gaudelet',
+    phone_number: '0102030405',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
