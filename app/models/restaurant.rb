a0395fb76d1438ad00e182b2e749538eef120778
hugo sola <hugo.degeorges@gmail.com>
2018-05-21 10:49:24 +0200

    okok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, acceptance: { accept: ["chinese", "english", "italian", "japanese", "french", "belgian"] }
 end
